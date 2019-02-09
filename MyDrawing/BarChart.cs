794f6b61ca59001e5b6ea7be29bbaf56fb4a1a5c
Medoed <knife491@gmail.com>
2019-02-09 20:18:54 +0200

    Добавлен метод для рисования столбцов гистограммы

--- MyDrawing/BarChart.cs
+++ MyDrawing/BarChart.cs
@@ -21,6 +21,8 @@ namespace MyDrawing
         public Color DiagramColor { get; set; }
         public Pen DiagramPen { get; set; }
         public Font drawFont { get; set; }
+        public SolidBrush drawBrush{get; set;}
+        public const byte HEIGHT = 4;
     }
 
     /// <summary>
@@ -60,7 +62,7 @@ namespace MyDrawing
             Config.DiagramColor = Color.Black;
             Config.DiagramPen = new Pen(Config.DiagramColor);
             Config.drawFont = new Font("Arial", 8);
-            
+            Config.drawBrush = new SolidBrush(Config.DiagramColor);
             //координаты угловых точек рамки
             //левая нижняя точка
             pt1 = new Point(Space_From_Left, placeToDraw.Height - Space_From_Bottom);
@@ -78,6 +80,8 @@ namespace MyDrawing
         {
             if(BarCollection.Count != 0)
             {
+                Config.BarWidth = 0;
+                Config.StepOX = 0;
                 //определение параметров по умолчанию для оси ОХ
                 while (Center.X + BarCollection.Count * (Config.BarWidth + Config.StepOX) + Config.StepOX <= pt3.X)
                 {
@@ -131,6 +135,49 @@ namespace MyDrawing
             //прорисовка осей без делений
             g.DrawLine(Config.DiagramPen, Center, LastPointOX); //ось ОХ
             g.DrawLine(Config.DiagramPen, Center, LastPointOY); //ось OY
+            g.DrawString("0", Config.drawFont, Config.drawBrush, Center.X - 6, Center.Y);
+
+            //прорисовка делений на оси OY
+            Point[] Oypoints1 = new Point[Config.NumberOfSepOY];
+            Point[] Oypoints2 = new Point[Config.NumberOfSepOY];
+            for(int i = 0; i < Oypoints1.Length; i++)
+            {
+                string num = Convert.ToString(i * Config.PriceForPointOY + Config.PriceForPointOY);
+                if (i == 0)
+                {
+                    Oypoints1[i].X = Center.X - BarChartConfig.HEIGHT;
+                    Oypoints1[i].Y = Center.Y - Config.StepOY;
+
+                    Oypoints2[i].X = Center.X + BarChartConfig.HEIGHT;
+                    Oypoints2[i].Y = Center.Y - Config.StepOY;
+                    g.DrawString(num, Config.drawFont, Config.drawBrush, Oypoints1[i].X - 10, Oypoints1[i].Y);
+                }
+                else
+                {
+                    Oypoints1[i].X = Center.X - BarChartConfig.HEIGHT;
+                    Oypoints1[i].Y = Oypoints1[i - 1].Y - Config.StepOY;
+
+                    Oypoints2[i].X = Center.X + BarChartConfig.HEIGHT;
+                    Oypoints2[i].Y = Oypoints2[i - 1].Y - Config.StepOY;
+                    g.DrawString(num, Config.drawFont, Config.drawBrush, Oypoints1[i].X - 10, Oypoints1[i].Y);
+                }
+                g.DrawLine(Config.DiagramPen, Oypoints1[i], Oypoints2[i]);
+            }
+        }
+
+        public void DrawCurrentBar()
+        {
+            for(int i = 0; i < BarCollection.Count; i++)
+            {
+                PointF BarPoint = new PointF();
+                BarPoint.X = i * Config.BarWidth + (i * Config.StepOX + Config.StepOX);
+                BarPoint.Y = (float)(Center.Y - BarCollection[i].BarValue * Config.StepOY / Config.PriceForPointOY);
+
+                RectangleF BarRectangle = new RectangleF(BarPoint.X, BarPoint.Y, Config.BarWidth, Center.Y - BarPoint.Y);
+
+                g.FillRectangle(new SolidBrush(BarCollection[i].BarColor), BarRectangle);
+                g.DrawString(BarCollection[i].BarName, Config.drawFont, Config.drawBrush, BarPoint.X, Center.Y);
+            }
         }
 
         public override void DrawGraphic()
